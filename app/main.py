c83a0c64069772a61ba7ecfc678eecee7637a5e0
Samuel Bedoya <samuelbedoya@Samuels-MacBook-Pro.local>
2025-06-08 03:31:30 -0500

    Corrected failures in .env, created method to get geolocation from ip provided and called method from main

--- app/main.py
+++ app/main.py
@@ -4,4 +4,8 @@ from app.routes.weather_route import router as weather_router
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the Weather API"}
+
 app.include_router(weather_router)
--- app/services/geolocation.py
+++ app/services/geolocation.py
@@ -1,6 +1,6 @@
 # app/services/geolocation.py
 import httpx # make async requests
-from app.config import GEO_API_KEY
+from app.config import GEO_API_KEY # call this from .env file
 
 async def get_location_by_ip(ip: str):
     url = f"https://geo.ipify.org/api/v2/country,city?apiKey={GEO_API_KEY}&ipAddress={ip}"
