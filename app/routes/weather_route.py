2f233047c94ac33973010d9ef2705715cef7d51f
Samuel Bedoya <samuelbedoya@Samuels-MacBook-Pro.local>
2025-06-08 04:09:50 -0500

    Method to see the weather with latitude and longitude created

--- app/routes/weather_route.py
+++ app/routes/weather_route.py
@@ -1,10 +1,16 @@
 # app/routes/weather_route.py
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from app.services.geolocation import get_location_by_ip
+from app.services.weather import get_weather_by_location
 
 router = APIRouter()
 
-@router.get("/weather/{ip}")
-async def get_weather(ip: str):
+@router.get("/location/{ip}")# get the location by ip
+async def get_location(ip: str):
     location = await get_location_by_ip(ip)
     return location
+
+@router.get("/weather/{ip}")# get the weather by location
+async def get_weather(lat: float, lon: float):
+    weather = await get_weather_by_location(lat, lon)
+    return weather
